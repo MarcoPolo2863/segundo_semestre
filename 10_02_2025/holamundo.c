cbf9206c2fe51a466cd6731936305b89f60aa1d9
root <root@MarcoLap.>
2025-02-13 20:31:33 -0600

    first commit

---
+++ 10_02_2025/holamundo.c
@@ -0,0 +1,12 @@
+#include <stdio.h> //directiva del compilador
+
+void holamundo();
+
+void main(){
+    holamundo();
+}
+
+//Funciones
+void holamundo(){
+    printf("Hola Mundo");
+} 
---
+++ 10_02_2025/nombre.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+void nombre();
+void paterno();
+void materno();
+
+void main(){
+    nombre();
+    paterno();
+    materno();
+}
+
+void nombre(){
+    printf("Marco Antonio ");
+}
+
+void paterno(){
+    printf("Flores ");
+}
+
+void materno(){
+    printf("Orozco ");
+}
