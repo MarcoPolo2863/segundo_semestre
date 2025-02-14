675d6e645d60a2ff32e6f44f940e9b529de0868f
MarcoPolo2863 <fakebull2863@outlook.com>
2025-02-13 21:00:56 -0600

    Update main2.c

--- main2.c
+++ main2.c
@@ -1,11 +1,9 @@
-// gcc main2.c
-// ./a.out
 
 #include <stdio.h>
-// C es lenguaje tipo tipado
-void main(){ // ambito o scope de variables/funciones
-    int numero_1;
-    int numero_2 = 5;   // identificador 
 
-    printf("%d", numero_1 * numero_2); // interpolacion de cadenas 
+void main(){
+    int numero_1;
+    int numero_2 = 5; 
+
+    printf("%d", numero_1 * numero_2); 
 }
